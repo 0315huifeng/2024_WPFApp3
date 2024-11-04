2bfad160a6e34869c74b42c856f3c499a310ba16
0315huifeng <4b2g0045@stust.edu.tw>
2024-11-05 01:19:10 +0800

    加入專案檔案。

---
+++ 2024_WPFApp3/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace _2024_WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var targetTextBox = sender as TextBox;
+            int amount;
+            bool success = int.TryParse(targetTextBox.Text, out amount);
+            if (!success)
+            {
+                MessageBox.Show("請輸入正整數", "輸入錯誤");
+            }
+            else if (amount <= 0)
+            {
+                MessageBox.Show("請輸入正整數", "輸入錯誤");
+            }
+            else
+            {
+                var targetStackPanel = targetTextBox.Parent as StackPanel;
+                var targetLabel = targetStackPanel.Children[0] as Label;
+                var drinkName = targetLabel.Content.ToString();
+                ResultTextBlock.Text += $"您選擇了{drinkName}，數量為{amount}杯\n";
+            }
+        }
+    }
+}
