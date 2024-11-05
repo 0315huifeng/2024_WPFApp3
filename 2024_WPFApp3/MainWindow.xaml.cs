b963fa0e5eecc943754a4568048a6ae954373fe0
0315huifeng <4b2g0045@stust.edu.tw>
2024-11-05 09:41:58 +0800

    - 將 `MainWindow.xaml` 中的標題從 "飲料點餐系統Ver3" 改為 "飲料點餐系統Ver4" - 新增 `MinWidth="600"` 屬性至 `MainWindow.xaml` - 修改 `Grid` 元素的第一個 `ColumnDefinition` 寬度為 `400*` 並新增 `MinWidth="400"` - 在 `MainWindow.xaml.cs` 中新增 `using System.IO;` - 在 `AddNewDrink` 方法中新增 `openFileDialog.Title = "選擇飲料菜單檔案";` - 實作 `ReadDrinksFromFile` 方法，讀取檔案並將飲料名稱和價格加入 `drinks` 字典 - 在 `RadioButton_Checked` 方法和 `for` 迴圈中移除多餘的空格 - 顯示訂單細項時新增訂購時間，並將訂單訊息儲存在 `orderMessage` 字串中 - 新增 `SaveOrder` 方法，將訂單訊息儲存到檔案中，並在成功或失敗時顯示相應的訊息 - 在 `drinkitem.csv` 中新增 "可樂大杯,30"

--- 2024_WPFApp3/MainWindow.xaml.cs
+++ 2024_WPFApp3/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,26 +12,51 @@ namespace _2024_WpfApp3
     /// </summary>
     public partial class MainWindow : Window
     {
-        Dictionary<string, int> drinks = new Dictionary<string, int>
-        {
-            {"紅茶大杯" ,60},
-            {"紅茶小杯" ,30},
-            {"綠茶大杯" ,55},
-            {"綠茶小杯" ,35},
-            {"奶茶大杯" ,70},
-            {"奶茶小杯" ,60},
-        };
-
+        Dictionary<string, int> drinks = new Dictionary<string, int>();
         Dictionary<string, int> orders = new Dictionary<string, int>();
         string takeout = "";
+
         public MainWindow()
         {
             InitializeComponent();
 
-            DisplayDrinksMenu(drinks);
+            AddNewDrink(drinks);
+
+            DisplayDrinkMenu(drinks);
         }
 
-        private void DisplayDrinksMenu(Dictionary<string, int> drinks)
+        private void AddNewDrink(Dictionary<string, int> drinks)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "CSV檔案|*.csv|文字檔案|*.txt|所有檔案|*.*";
+            openFileDialog.Title = "選擇飲料菜單檔案";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string fileName = openFileDialog.FileName;
+                ReadDrinksFromFile(fileName, drinks);
+            }
+        }
+
+        private void ReadDrinksFromFile(string fileName, Dictionary<string, int> drinks)
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(fileName);
+                foreach (var line in lines)
+                {
+                    string[] tokens = line.Split(',');
+                    string drinkName = tokens[0];
+                    int price = Convert.ToInt32(tokens[1]);
+                    drinks.Add(drinkName, price);
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show($"讀取檔案時發生錯誤: {ex.Message}");
+            }
+        }
+
+        private void DisplayDrinkMenu(Dictionary<string, int> drinks)
         {
             stackpanel_DrinkMenu.Children.Clear();
             stackpanel_DrinkMenu.Height = drinks.Count * 40;
@@ -40,9 +66,9 @@ namespace _2024_WpfApp3
                 {
                     Orientation = Orientation.Horizontal,
                     Margin = new Thickness(2),
-                    Height =35,
+                    Height = 35,
                     VerticalAlignment = VerticalAlignment.Center,
-                    Background = Brushes.AliceBlue,
+                    Background = Brushes.AliceBlue
                 };
 
                 var cb = new CheckBox
@@ -51,8 +77,8 @@ namespace _2024_WpfApp3
                     FontFamily = new FontFamily("微軟正黑體"),
                     FontSize = 18,
                     Foreground = Brushes.Blue,
-                    Margin = new Thickness (10,0,40,0),
-                    VerticalContentAlignment = VerticalAlignment.Center,
+                    Margin = new Thickness(10, 0, 40, 0),
+                    VerticalContentAlignment = VerticalAlignment.Center
                 };
 
                 var sl = new Slider
@@ -68,21 +94,22 @@ namespace _2024_WpfApp3
                 var lb = new Label
                 {
                     Width = 30,
-                    Content = 0,
+                    Content = "0",
                     FontFamily = new FontFamily("微軟正黑體"),
                     FontSize = 18,
                 };
 
-                Binding mybinding = new Binding("Value")
+                Binding myBinding = new Binding("Value")
                 {
                     Source = sl,
-                    Mode = BindingMode.OneWay,
+                    Mode = BindingMode.OneWay
                 };
-                lb.SetBinding(ContentProperty, mybinding);
+                lb.SetBinding(ContentProperty, myBinding);
 
                 sp.Children.Add(cb);
                 sp.Children.Add(sl);
                 sp.Children.Add(lb);
+
                 stackpanel_DrinkMenu.Children.Add(sp);
             }
         }
@@ -90,12 +117,10 @@ namespace _2024_WpfApp3
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
             var rb = sender as RadioButton;
+            if ((rb.IsChecked == true))
             {
-                if (rb.IsChecked == true) 
-                {
-                    takeout = rb.Content.ToString();
-                    MessageBox.Show($"方式: {takeout}");
-                }
+                takeout = rb.Content.ToString();
+                //MessageBox.Show($"方式: {takeout}");
             }
         }
 
@@ -103,7 +128,7 @@ namespace _2024_WpfApp3
         {
             ResultTextBlock.Text = "";
             string discoutMessage = "";
-            //確認所有訂單的品項
+            // 確認所有訂單的品項
             orders.Clear();
             for (int i = 0; i < stackpanel_DrinkMenu.Children.Count; i++)
             {
@@ -111,20 +136,26 @@ namespace _2024_WpfApp3
                 var cb = sp.Children[0] as CheckBox;
                 var sl = sp.Children[1] as Slider;
                 var lb = sp.Children[2] as Label;
+
                 if (cb.IsChecked == true && sl.Value > 0)
                 {
                     string drinkName = cb.Content.ToString().Split(' ')[0];
                     orders.Add(drinkName, int.Parse(lb.Content.ToString()));
                 }
             }
-            //顯示訂單細項，並計算總金額
+
+            // 顯示訂單細項，並計算總金額
             double total = 0.0;
             double sellPrice = 0.0;
 
-            ResultTextBlock.Text += $"取餐方式: {takeout}\n";
+            string orderMessage = "";
+            DateTime now = DateTime.Now;
+            orderMessage += $"訂購時間: {now.ToString("yyyy/MM/dd HH:mm:ss")}\n";
+            orderMessage += $"取餐方式: {takeout}\n"; ;
 
-            int num = 1 ;
-            foreach (var item in orders) 
+            int num = 1;
+
+            foreach (var item in orders)
             {
                 string drinkName = item.Key;
                 int quantity = item.Value;
@@ -132,14 +163,14 @@ namespace _2024_WpfApp3
 
                 int subTotal = price * quantity;
                 total += subTotal;
-                ResultTextBlock.Text += $"{num}. {drinkName} x {quantity}杯，共{subTotal}元\n";
+                orderMessage += $"{num}. {drinkName} x {quantity}杯，共{subTotal}元\n";
                 num++;
             }
 
-            if(total >=500)
+            if (total >= 500)
             {
-                discoutMessage = "滿500打8折";
-                sellPrice = total*0.8;
+                discoutMessage = "滿500元打8折";
+                sellPrice = total * 0.8;
             }
             else if (total >= 300)
             {
@@ -152,8 +183,34 @@ namespace _2024_WpfApp3
                 sellPrice = total;
             }
 
-            ResultTextBlock.Text += $"總金額: {total}元\n";
-            ResultTextBlock.Text += $"{discoutMessage}，實付金額: {sellPrice}元\n";
+            orderMessage += $"總金額: {total}元\n";
+            orderMessage += $"{discoutMessage}，實付金額: {sellPrice}元\n";
+            ResultTextBlock.Text = orderMessage;
+            SaveOrder(orderMessage);
+        }
+
+        private void SaveOrder(string orderMessage)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "文字檔案|*.txt|所有檔案|*.*";
+            saveFileDialog.Title = "儲存訂單";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string fileName = saveFileDialog.FileName;
+
+                try
+                {
+                    using (StreamWriter sw = new StreamWriter(fileName))
+                    {
+                        sw.Write(orderMessage);
+                    }
+                    MessageBox.Show("訂單已成功儲存。");
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show($"儲存檔案時發生錯誤: {ex.Message}");
+                }
+            }
         }
     }
 }
